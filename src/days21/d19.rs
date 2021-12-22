e25aeec694661d0d682088b46cb9ede9926af6fa
mikatpt <mikatpt@gmail.com>
2021-12-22 00:58:21 -0500

    not great days

---
+++ src/days21/d19.rs
@@ -0,0 +1,113 @@
+use crate::Result;
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
+
+type Scanners = Vec<Vec<(i32, i32, i32)>>;
+
+fn read(input: &str) -> Scanners {
+    let b = |line: &str| {
+        line.split(',')
+            .map(str::parse::<i32>)
+            .map(Result::unwrap)
+            .collect_tuple()
+            .unwrap()
+    };
+    let a = |scanner: &str| scanner.lines().skip(1).map(b).collect();
+    input.split("\n\n").map(a).collect()
+}
+
+fn rotate_90((x, y, z): (i32, i32, i32)) -> [(i32, i32, i32); 4] {
+    [
+        (x, y, z),   // normal case
+        (x, -z, y),  // rotate left 90 degrees
+        (x, -y, -z), // rotate 180 degrees
+        (x, z, -y),  // rotate right 90 degrees
+    ]
+}
+
+const DIRS: [(i32, i32, i32); 8] = [
+    (1, 1, 1),
+    (1, -1, 1),
+    (1, 1, -1),
+    (1, -1, -1),
+    (-1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+];
+
+const ROT: [(i32, i32, i32); 4] = [(1, 1, 1), (1, -1, 1), (1, -1, -1), (1, 1, -1)];
+
+// this doesn't work :(
+fn find_beacons(scanners: Scanners) -> HashSet<(i32, i32, i32)> {
+    let mut beacons = HashSet::from_iter(scanners[0].clone());
+
+    'outer: for scanned in scanners.into_iter().skip(1) {
+        let mut counts = HashMap::<(i32, i32, i32), i32>::new();
+
+        for &(bx, by, bz) in &scanned {
+            for &beacon in &beacons {
+                for (i, (ax, ay, az)) in rotate_90(beacon).iter().enumerate() {
+                    // let (ax, ay, az) = (beacon.0 * rx, beacon.1 * ry, beacon.2 * rz);
+                    // println!("rotated: ({ax},{ay},{az})");
+                    for (dx, dy, dz) in DIRS {
+                        // formula: scannerX = bx + rotate(ax) * dx
+                        let (sx, sy, sz) = (bx * dx + ax, by * dy + ay, bz * dz + az);
+                        // if sx == 68 && sy == -1246 && sz == -43 {
+                        //     println!("this iteration should have it\n");
+                        // }
+                        let count = counts.entry((sx, sy, sz)).or_default();
+                        *count += 1;
+                        if *count > 11 {
+                            println!("\nfound scanner, it's at ({},{},{})", sx, sy, sz);
+                            println!("dirs are at ({},{},{})", dx, dy, dz);
+                            println!("rotated are at ({},{},{})", ax, ay, az);
+                            // if found scanner: for all scanned, solve for a and insert to beacons
+                            // formula: ax = (scannerx - bx) / direction
+                            for &(cx, cy, cz) in &scanned {
+                                let (cx, cy, cz) = rotate_90((cx, cy, cz))[i];
+                                let transformed_a_coords =
+                                    (sx - cx * dx, sy - cy * dy, sz - cz * dz);
+                                beacons.insert(transformed_a_coords);
+                            }
+                            continue 'outer;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    beacons
+}
+
+pub fn part1(input: &str) -> Result<usize> {
+    let scanners = read(input);
+    let mut beacons: Vec<(i32, i32, i32)> = find_beacons(scanners).into_iter().collect();
+    beacons.sort_unstable();
+    println!("beacons: {:?}", beacons);
+
+    Ok(beacons.len())
+}
+
+pub fn part2(input: &str) -> Result<i32> {
+    Ok(0)
+}
+
+const INPUT: &str = include_str!("../../input/21/19_test.txt");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use eyre_test::test;
+
+    #[test]
+    fn test1() {
+        assert_eq!(79, part1(INPUT).unwrap());
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(0, part2(INPUT).unwrap());
+    }
+}
---
+++ src/days21/d21.rs
@@ -0,0 +1,62 @@
+use crate::Result;
+use itertools::Itertools;
+
+fn deterministic(mut p1: u64, mut p2: u64) -> u64 {
+    let (mut p1_s, mut p2_s) = (0, 0);
+    let mut current = &mut p1;
+    let mut score = &mut p1_s;
+    let mut rolls = 1;
+
+    loop {
+        *current = ((3 * rolls + 3 + *current - 1) % 10) + 1;
+        *score += *current;
+        rolls += 3;
+        if *score > 999 {
+            break;
+        }
+        current = if *current == p1 { &mut p2 } else { &mut p1 };
+        score = if *score == p1_s { &mut p2_s } else { &mut p1_s };
+    }
+    rolls -= 1;
+
+    let loser = if *score == p1_s { p2_s } else { p1_s };
+    println!("loser:{},rolls:{}", loser, rolls);
+    loser * rolls
+}
+
+fn read(input: &str) -> (u64, u64) {
+    input
+        .lines()
+        .map(|line| line.chars().last().unwrap())
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .collect_tuple()
+        .unwrap()
+}
+pub fn part1(input: &str) -> Result<u64> {
+    let (p1, p2) = read(input);
+    Ok(deterministic(p1, p2))
+}
+
+pub fn part2(input: &str) -> Result<usize> {
+    Ok(0)
+}
+
+const INPUT: &str = "p1 start: 7\np2 start: 9";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use eyre_test::test;
+
+    #[test]
+    fn test1() {
+        assert_eq!(739785, part1("p1 start: 4\np2 start: 8").unwrap());
+        println!("{}", part1(INPUT).unwrap());
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(444356092776315, part2("p1 start: 4\np2 start: 8").unwrap());
+        assert_eq!(0, part2(INPUT).unwrap());
+    }
+}
